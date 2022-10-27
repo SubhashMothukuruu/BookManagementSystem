a5e8bf1d40aadb402339ecf05baf54f02a18b17b
SubhashMOthukuri <86213950+SubhashMOthukuri@users.noreply.github.com>
2022-10-27 17:10:22 +0530

    first commit

---
+++ src/main/java/com/NewManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewManagementSystemApplication.class, args);
+	}
+
+}
